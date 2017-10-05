8c4c3ad3dba928adffae3635c819f6f4f2cf50e2
Carl Larson <cslarson@gmail.com>
2017-10-05 22:03:17 +0100

    use postgres instead of sqlite for regbot

--- bot/regbot.py
+++ bot/regbot.py
@@ -1,19 +1,30 @@
 import praw
 import time
-import sqlite3
-conn = sqlite3.connect('regbot.db')
+import psycopg2
+import argparse
+
+conn_string = "host='localhost' dbname='reddit' user='postgres' password=''"
+conn = psycopg2.connect(conn_string)
+cursor = conn.cursor()
 
 reddit = praw.Reddit()
-c = conn.cursor()
 
-subreddit = reddit.subreddit('ethtrader_test')
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--sub', help='the subreddit to watch')
+args = vars(parser.parse_args())
+
+subreddit = reddit.subreddit(args['sub'])
+print("streaming from:", subreddit)
 comments = subreddit.stream.comments()
 
 for comment in comments:
     text = comment.body # Fetch body
     author = comment.author # Fetch author
     if '!ethreg' in text.lower():
-        c.execute("INSERT INTO comments (comment_id, replied) VALUES (?, 0)", (comment.id,))
-        conn.commit()
+        try:
+            cursor.execute("INSERT INTO reg_comments (comment_id) VALUES (%s)", (comment.id,))
+            conn.commit()
+        except Exception as err:
+            print(err)
 
 conn.close()
--- bot/regbot_replier.py
+++ bot/regbot_replier.py
@@ -1,16 +1,20 @@
 import praw
 import time
-import sqlite3
-conn = sqlite3.connect('regbot.db')
+import psycopg2
+
+conn_string = "host='localhost' dbname='reddit' user='postgres' password=''"
+conn = psycopg2.connect(conn_string)
+cursor = conn.cursor()
 
 reddit = praw.Reddit()
-c = conn.cursor()
 
 while True:
-    for (comment_id,) in c.execute("SELECT comment_id FROM comments WHERE replied = 0"):
+    cursor.execute("SELECT comment_id FROM reg_comments WHERE replied = false ORDER BY created_at ASC")
+    unreplied = cursor.fetchall()
+    for comment_id in [i[0] for i in unreplied]:
         comment = reddit.comment(comment_id)
         comment.reply("your karma is x")
-        c.execute("UPDATE comments SET replied = 1 WHERE comment_id=?", (comment_id,))
+        cursor.execute("UPDATE reg_comments SET replied = true WHERE comment_id = %s", (comment_id,))
         conn.commit()
         print(comment_id)
         time.sleep(2)   # no more than 1 reply every 2s
